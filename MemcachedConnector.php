bc32f8a0fe644bf371054c5a1f0b5bdb7a8830af
Krishna Pennacchioni <krishna@agentelinux.com.br>
2017-01-11 22:21:31 -0200

    Update MemcachedConnector.php
    compatibility with Laravel 5.3

--- MemcachedConnector.php
+++ MemcachedConnector.php
@@ -22,17 +22,23 @@ class MemcachedConnector extends FrameworkMemcachedConnector
      *
      * @throws \RuntimeException
      */
-    public function connect(array $servers)
-    {
-        $memcached = $this->getMemcached();
+    public function connect(
+        array $servers, $connectionId = null,
+        array $options = [], array $credentials = []
+    ) {
+        $memcached = $this->getMemcached(
+            $connectionId, $credentials, $options
+        );
 
-        // For each server in the array, we'll just extract the configuration and add
-        // the server to the Memcached connection. Once we have added all of these
-        // servers we'll verify the connection is successful and return it back.
-        foreach ($servers as $server) {
-            $memcached->addServer(
-                $server['host'], $server['port'], $server['weight']
-            );
+        if (! $memcached->getServerList()) {
+            // For each server in the array, we'll just extract the configuration and add
+            // the server to the Memcached connection. Once we have added all of these
+            // servers we'll verify the connection is successful and return it back.
+            foreach ($servers as $server) {
+                $memcached->addServer(
+                    $server['host'], $server['port'], $server['weight']
+                );
+            }
         }
 
         $memcached->set('l5-noversion', 1);
