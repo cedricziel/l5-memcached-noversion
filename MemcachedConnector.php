52aabb528cd2a8e39c082aee5d397a8e77b38e0d
Krishna Pennacchioni <krishna@agentelinux.com.br>
2017-01-12 00:49:46 -0200

    Update MemcachedConnector.php

--- MemcachedConnector.php
+++ MemcachedConnector.php
@@ -3,7 +3,7 @@
 namespace CedricZiel\MemcachedNoVersion;
 
 use Illuminate\Cache\MemcachedConnector as FrameworkMemcachedConnector;
-use Memcache;
+use Memcached;
 use RuntimeException;
 
 /**
@@ -26,7 +26,7 @@ class MemcachedConnector extends FrameworkMemcachedConnector
         array $servers, $connectionId = null,
         array $options = [], array $credentials = []
     ) {
-        $memcached = new Memcache;
+        $memcached = new Memcached;
         // For each server in the array, we'll just extract the configuration and add
         // the server to the Memcached connection. Once we have added all of these
         // servers we'll verify the connection is successful and return it back.
